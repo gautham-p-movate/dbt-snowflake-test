100ce92467cf0747e6daf26d3ee467e4d1ebcc7f
Gautham.Periyasamy@movate.com <Gautham.Periyasamy@movate.com>
2025-12-08 05:45:58 +0000

    fixes

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -10,15 +10,11 @@
 {{ config(materialized='view') }}
 
 with source_data as (
-
     select 1 as id
     union all
     select null as id
-
 )
-
-select *
-from source_data where id is not null;
+select * from source_data where id is not null;
 /*
     Uncomment the line below to remove records with null `id` values
 */
