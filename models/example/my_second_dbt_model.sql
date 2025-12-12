160e0e334965070f9a35e9a14c5656b98e721961
Gautham Periyasamy <Gautham.Periyasamy@movate.com>
2025-12-12 10:48:15 +0000

    Updated stat to status

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select appl_status as stat
+select appl_status as status
 from {{ ref('my_first_dbt_model') }}
 where appl_status = 'Active'
--- models/process/filter_model.sql
+++ models/process/filter_model.sql
@@ -1,3 +1,3 @@
 select *
 from {{ ref('bureau_model') }}
-where credit_active = (Select stat from {{ ref('my_second_dbt_model') }})
+where credit_active = (Select status from {{ ref('my_second_dbt_model') }})
